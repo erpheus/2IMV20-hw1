a6a5f90ae6c768cc5725bee9d9c38377f7fca4bf
RF <rfmrd619@gmail.com>
2018-11-25 03:22:14 +0100

    compute gradient in GradientVolume

--- src/volume/GradientVolume.java
+++ src/volume/GradientVolume.java
@@ -10,6 +10,12 @@ package volume;
  */
 public class GradientVolume {
 
+    private int dimX, dimY, dimZ;
+    private VoxelGradient zero = new VoxelGradient();
+    VoxelGradient[] data;
+    Volume volume;
+    double maxmag;
+
     public GradientVolume(Volume vol) {
         volume = vol;
         dimX = vol.getDimX();
@@ -55,8 +61,54 @@ public class GradientVolume {
         for (int i=0; i<data.length; i++) {
             data[i] = zero;
         }
+
+        for (int x=0;x<dimX; x++) {
+            for (int y=0; y<dimY; y++) {
+                for (int z=0; z<dimZ; z++) {
+                    double xMinus = 0;
+                    double xPlus = 0;
+                    double yMinus = 0;
+                    double yPlus = 0;
+                    double zMinus = 0;
+                    double zPlus = 0;
+
+                    if (x > 0) {
+                        xMinus = volume.getVoxel(x-1, y, z);
+                    }
+
+                    if (x < dimX - 1) {
+                        xPlus = volume.getVoxel(x+1, y, z);
+                    }
+
+                    if (y > 0) {
+                        yMinus = volume.getVoxel(x, y-1, z);
+                    }
+
+                    if (y < dimY - 1) {
+                        yPlus = volume.getVoxel(x, y+1, z);
+                    }
+
+                    if (z > 0) {
+                        zMinus = volume.getVoxel(x, y, z-1);
+                    }
+
+                    if (z < dimZ - 1) {
+                        zPlus = volume.getVoxel(x, y, z+1);
+                    }
+
+                    int xGradient = (int) (0.5 *(xPlus-xMinus));
+                    int yGradient = (int) (0.5 *(yPlus-yMinus));
+                    int zGradient = (int) (0.5 *(zPlus-zMinus));
+
+                    VoxelGradient gradient =  new VoxelGradient(xGradient, yGradient, zGradient);
+                    data[x + dimX * (y + dimY * z)] = gradient;
+
+                }
+            }
+        }
                 
     }
+
     
     public double getMaxGradientMagnitude() {
         if (maxmag >= 0) {
@@ -71,9 +123,5 @@ public class GradientVolume {
         }
     }
     
-    private int dimX, dimY, dimZ;
-    private VoxelGradient zero = new VoxelGradient();
-    VoxelGradient[] data;
-    Volume volume;
-    double maxmag;
+
 }
