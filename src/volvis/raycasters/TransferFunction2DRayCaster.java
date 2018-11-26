1f152f33b7eeb99fc01b6297eb0de09ef2b96650
Erpheus <erpheus@gmail.com>
2018-11-26 11:56:56 +0100

    Updated transfer2draycaster to new baseraycaster interface

--- src/volvis/raycasters/TransferFunction2DRayCaster.java
+++ src/volvis/raycasters/TransferFunction2DRayCaster.java
@@ -11,7 +11,14 @@ import java.awt.image.BufferedImage;
 public class TransferFunction2DRayCaster extends BaseRaycaster {
 
     @Override
-    public void cast(double[] viewMatrix) {
+    protected int displacementForRunningTime(long lastRunningTime, int lastDisplacement) {
+        // we use the cube root to update the displacement as it has a cubic relationship with the rendering time
+        // because it is used inside three nested loops during rendering.
+        return (int) Math.ceil(lastDisplacement * Math.cbrt((double)lastRunningTime / TARGET_RENDERING_TIME));
+    }
+
+    @Override
+    public void internal_cast(double[] viewMatrix) {
         castSetUp(viewMatrix);
 
         double[] pixelCoord = new double[3];
@@ -24,14 +31,15 @@ public class TransferFunction2DRayCaster extends BaseRaycaster {
 
         for (int j = 0; j < image.getHeight(); j++) {
             for (int i = 0; i < image.getWidth(); i++) {
-                double k = +1 * (maximumSteps / 2);
+                double k = +1 * (maximumSteps / 2) - displacement/2 ;
+                double min_k = displacement/2 + (-1 * (maximumSteps / 2));
 
                 voxelColor = new TFColor(0,0,0,1);
-                while(k >= -1*maximumSteps/2) {//Math.max(volume.getDimX(), Math.max(volume.getDimY(), volume.getDimZ()))) {
+                while(k >= min_k) {//Math.max(volume.getDimX(), Math.max(volume.getDimY(), volume.getDimZ()))) {
                     pixelCoord[0] = uVec[0] * (i - imageCenter) + vVec[0] * (j - imageCenter) + volumeCenter[0] + k*viewVec[0];
                     pixelCoord[1] = uVec[1] * (i - imageCenter) + vVec[1] * (j - imageCenter) + volumeCenter[1] + k*viewVec[1];
                     pixelCoord[2] = uVec[2] * (i - imageCenter) + vVec[2] * (j - imageCenter) + volumeCenter[2] + k*viewVec[2];
-                    k--;
+                    k-= displacement;
                     double val = interpVoxels(pixelCoord);
                     currentVoxelColor = tFunc.getColor((int)(val));
                     voxelColor.composite(currentVoxelColor);
